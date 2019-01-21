fe65123b02ae92406ab0ff534d07677525f64882
Bruce's Thinkpad <brucezhaor2016@gmail.com>
2019-01-21 23:50:15 +0800

    fix typo

--- pkgdown/deploy.sh
+++ pkgdown/deploy.sh
@@ -4,7 +4,7 @@ set -e
 git config --global user.email "brucezhaor2016@gmail.com"
 git config --global user.name "BruceZhaoR"
 
-git clone -q -b gh-pages https://${GITHUB_PAT}@github.com/brucezhaor/lightgbm_r.git output
+git clone -q -b gh-pages https://${GITHUB_PAT}@github.com/brucezhaor/rsixygen.git output
 cd output
 git rm -rf *
   cp -r ../docs/* ./
