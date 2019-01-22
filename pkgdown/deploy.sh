588405baaa373a09e3b0f832f10ca614499fb383
Bruce's Thinkpad <brucezhaor2016@gmail.com>
2019-01-22 21:33:00 +0800

    for PR

--- pkgdown/deploy.sh
+++ pkgdown/deploy.sh
@@ -1,10 +1,10 @@
 set -e
 
 [ -z "${GITHUB_PAT}" ] && exit 0
-git config --global user.email "brucezhaor2016@gmail.com"
-git config --global user.name "BruceZhaoR"
+git config --global user.email "jaylamb20@gmail.com"
+git config --global user.name "jameslamb"
 
-git clone -q -b gh-pages https://${GITHUB_PAT}@github.com/brucezhaor/rsixygen.git output
+git clone -q -b gh-pages https://${GITHUB_PAT}@github.com/jameslamb/rsixygen.git output
 cd output
 git rm -rf *
   cp -r ../docs/* ./
