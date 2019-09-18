63a24cb3e8fbabb501c592ea23b855286d22a114
James Lamb <jaylamb20@gmail.com>
2019-09-17 22:34:24 -0500

    please work

--- pkgdown/deploy.sh
+++ pkgdown/deploy.sh
@@ -4,27 +4,30 @@ set -e
 
 [ -z "${GITHUB_PAT}" ] && exit 0
 
-echo "configuring git"
+echo "[INFO] configuring git"
 git config --global user.email "jaylamb20@gmail.com"
 git config --global user.name "jameslamb"
 
-echo "creating temporary directory to build repo"
+echo "[INFO] creating temporary directory to build repo"
 export OUTPUT_DIR=$(pwd)/output
 mkdir -p ${OUTPUT_DIR}
 
-echo "cloning gh-pages branch of the repo"
+echo "[INFO] cloning gh-pages branch of the repo"
 git clone \
     -b gh-pages \
     https://${GITHUB_PAT}@github.com/jameslamb/rsixygen.git \
     ${OUTPUT_DIR}
-echo "cloned repo to '${OUTPUT_DIR}'"
+echo "[INFO] cloned repo to '${OUTPUT_DIR}'"
 
 cd ${OUTPUT_DIR}
 
+echo "[INFO] copying docs to output dir"
 cp -r ../docs/* ./
+
+echo "[INFO] checking files into the repo"
 git add --all *
 git commit -m "Update the site from appveyor"
 
-echo "Pushing to gh-pages branch"
+echo "[INFO] pushing to gh-pages branch"
 git push -q origin gh-pages
-echo "Successfully updated gh-pages branch"
+echo "[INFO] successfully updated gh-pages branch"
