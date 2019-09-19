a575f2a2a327ac5ba2243f19d580bb2746c0c37d
James Lamb <jaylamb20@gmail.com>
2019-09-18 22:52:53 -0500

    minor style changes, various fixes to CI (#22)

--- README.md
+++ README.md
@@ -9,11 +9,11 @@ There is [no built-in support for R6](https://github.com/klutometis/roxygen/issu
 
 This package is experimental, and implements a standard proposed by @cwschultz88 for documenting these objects.
 
-To be clear, it does NOT automatically generate full Roxygen docs the way that `roxygen2::roxygenize` does. Instead, it dumps Roxygen skeletons to a directory of files. You will still have to do some manual work after this, but the package cuts down on a LOT of copy-pasting, indentation fixes, and general sadness around missing curly braces.
+To be clear, it does NOT automatically generate full Roxygen docs the way that `roxygen2::roxygenize()` does. Instead, it dumps Roxygen skeletons to a directory of files. You will still have to do some manual work after this, but the package cuts down on a LOT of copy-pasting, indentation fixes, and general sadness around missing curly braces.
 
 ## Installation
 
-This package is not yet on CRAN. For now, just install from Github.
+This package is not yet on CRAN. For now, just install from GitHub.
 
 ```
 devtools::install_github('jameslamb/rsixygen')
--- pkgdown/deploy.sh
+++ pkgdown/deploy.sh
@@ -1,14 +1,35 @@
+
+# failure is a natural part of life
 set -e
 
 [ -z "${GITHUB_PAT}" ] && exit 0
+
+echo "[INFO] configuring git"
 git config --global user.email "jaylamb20@gmail.com"
 git config --global user.name "jameslamb"
 
-git clone -q -b gh-pages https://${GITHUB_PAT}@github.com/jameslamb/rsixygen.git output
-cd output
-git rm -rf *
-  cp -r ../docs/* ./
-  git add --all *
-  git commit -m"Update the site from appveyor"
-# echo add files to gh-pages
-git push -q origin gh-pages
+echo "[INFO] creating temporary directory to build repo"
+export OUTPUT_DIR=$(pwd)/output
+mkdir -p ${OUTPUT_DIR}
+
+echo "[INFO] cloning gh-pages branch of the repo"
+git clone \
+    -b gh-pages \
+    https://${GITHUB_PAT}@github.com/jameslamb/rsixygen.git \
+    ${OUTPUT_DIR}
+echo "[INFO] cloned repo to '${OUTPUT_DIR}'"
+
+cd ${OUTPUT_DIR}
+
+echo "[INFO] copying docs to output dir"
+cp -r ../docs/* ./
+
+echo "[INFO] checking files into the repo"
+git add --all *
+
+echo "[INFO]"
+git commit -m "Update the site from appveyor" || exit 0
+
+echo "[INFO] pushing to gh-pages branch"
+git push -q origin gh-pages || exit 0
+echo "[INFO] successfully updated gh-pages branch"
